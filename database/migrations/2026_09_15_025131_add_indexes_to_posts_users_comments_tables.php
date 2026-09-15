f36e763aad51c93691900ac8b010db9df565726b
ajitirto <ajiwoke@gmail.com>
2026-09-15 13:48:01 +0700

    refactor(db): update article indexes

--- database/migrations/2026_09_15_025131_add_indexes_to_posts_users_comments_tables.php
+++ database/migrations/2026_09_15_025131_add_indexes_to_posts_users_comments_tables.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        /* Schema::table('posts', function (Blueprint $table) { */
-        /*     $table->index(['tags', 'views']); */
-        /*     $table->index('user_id'); */
-        /* }); */
-        /**/
-        /* Schema::table('comments', function (Blueprint $table) { */
-        /*     $table->index('post_id'); */
-        /*     $table->index('user_id'); */
-        /* }); */
+        Schema::table('posts', function (Blueprint $table) {
+            $table->index(['tags', 'views']);
+            $table->index('user_id');
+        });
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->index('post_id');
+            $table->index('user_id');
+        });
     }
 
     public function down(): void
